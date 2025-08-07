bcc85d552f1d49af20eff8f89b53bfcabff4b4ef
nico <nico@example.com>
2025-08-08 01:55:49 +0800

    新增投资项目下单接口

--- renren-api/src/main/java/io/renren/controller/ApiProjectController.java
+++ renren-api/src/main/java/io/renren/controller/ApiProjectController.java
@@ -1,60 +1,74 @@
+package io.renren.controller;
+
+import io.renren.common.utils.Result;
+import io.renren.dto.ProjectDTO;
+import io.renren.dto.ProjectTypeDTO;
+import io.renren.service.ProjectService;
+import io.renren.service.ProjectTypeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+import io.renren.dto.PlaceOrderDTO;
+import io.renren.common.validator.ValidatorUtils;
+import org.springframework.web.bind.annotation.RequestBody;
+import java.util.Map;
+import java.util.HashMap;
+
 /**
- * Copyright (c) 2018 人人开源 All rights reserved.
+ * 投资项目接口
  *
- * https://www.renren.io
- *
- * 版权所有，侵权必究！
+ * @author Mark sunlightcs@gmail.com
  */
+@RestController
+@RequestMapping("/api/project")
+@Api(tags = "投资项目接口")
+public class ApiProjectController {
+    @Resource
+    private ProjectService projectService;
+    
+    @Resource
+    private ProjectTypeService projectTypeService;
 
- package io.renren.controller;
+    @GetMapping("listGroup")
+    @ApiOperation("投资项目列表")
+    public Result<List<ProjectDTO>> listGroup() {
+        List<ProjectDTO> list = projectService.queryListGroup();
+        return new Result<List<ProjectDTO>>().ok(list);
+    }
+    
+    @GetMapping("list")
+    @ApiOperation("投资项目类型列表")
+    public Result<List<ProjectTypeDTO>> list() {
+        List<ProjectTypeDTO> list = projectTypeService.queryList();
+        return new Result<List<ProjectTypeDTO>>().ok(list);
+    }
+    
+    @GetMapping("{id}")
+    @ApiOperation("投资项目信息")
+    public Result<ProjectDTO> getById(
+            @ApiParam(value = "项目ID", required = true) @PathVariable("id") Long id) {
+        ProjectDTO project = projectService.getById(id);
+        return new Result<ProjectDTO>().ok(project);
+    }
 
- import io.renren.common.utils.Result;
- import io.renren.dto.ProjectDTO;
- import io.renren.dto.ProjectTypeDTO;
- import io.renren.service.ProjectService;
- import io.renren.service.ProjectTypeService;
- import io.swagger.annotations.Api;
- import io.swagger.annotations.ApiOperation;
- import io.swagger.annotations.ApiParam;
- import org.springframework.web.bind.annotation.*;
- 
- import javax.annotation.Resource;
- import java.util.List;
- 
- /**
-  * 投资项目接口
-  *
-  * @author Mark sunlightcs@gmail.com
-  */
- @RestController
- @RequestMapping("/api/project")
- @Api(tags = "投资项目接口")
- public class ApiProjectController {
-     @Resource
-     private ProjectService projectService;
-     
-     @Resource
-     private ProjectTypeService projectTypeService;
- 
-     @GetMapping("listGroup")
-     @ApiOperation("投资项目列表")
-     public Result<List<ProjectDTO>> listGroup() {
-         List<ProjectDTO> list = projectService.queryListGroup();
-         return new Result<List<ProjectDTO>>().ok(list);
-     }
-     
-     @GetMapping("list")
-     @ApiOperation("投资项目类型列表")
-     public Result<List<ProjectTypeDTO>> list() {
-         List<ProjectTypeDTO> list = projectTypeService.queryList();
-         return new Result<List<ProjectTypeDTO>>().ok(list);
-     }
-     
-     @GetMapping("{id}")
-     @ApiOperation("投资项目信息")
-     public Result<ProjectDTO> getById(
-             @ApiParam(value = "项目ID", required = true) @PathVariable("id") Long id) {
-         ProjectDTO project = projectService.getById(id);
-         return new Result<ProjectDTO>().ok(project);
-     }
- }
+    @PostMapping("placeAnOrder")
+    @ApiOperation("下单")
+    public Result<Map<String, String>> placeAnOrder(@RequestBody PlaceOrderDTO dto) {
+        // 参数校验
+        ValidatorUtils.validateEntity(dto);
+        
+        // TODO: 实现下单逻辑
+        // 这里需要根据业务需求实现具体的下单逻辑
+        // 包括：验证用户余额、验证项目状态、创建订单、扣除余额等
+        
+        Map<String, String> result = new HashMap<>();
+        result.put("orderId", "ORDER_" + System.currentTimeMillis());
+        result.put("status", "success");
+        
+        return new Result<Map<String, String>>().ok(result);
+    }
+}
---
+++ renren-api/src/main/java/io/renren/dto/PlaceOrderDTO.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2018 人人开源 All rights reserved.
+ *
+ * https://www.renren.io
+ *
+ * 版权所有，侵权必究！
+ */
+
+package io.renren.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 下单表单
+ *
+ * @author Mark sunlightcs@gmail.com
+ * @since 1.0.0
+ */
+@Data
+@ApiModel(value = "下单表单")
+public class PlaceOrderDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "投资总金额(优惠价无需计算，后端做处理 单位分)")
+    private Long amount;
+
+    @ApiModelProperty(value = "channel")
+    private String channel;
+
+    @ApiModelProperty(value = "购买份数")
+    private Integer count;
+
+    @ApiModelProperty(value = "是否使用优惠券(1使用 2不使用)")
+    private Integer couponType;
+
+    @ApiModelProperty(value = "gift")
+    private Integer gift;
+
+    @ApiModelProperty(value = "投资理财项id")
+    @NotNull(message = "投资理财项id不能为空")
+    private Long investId;
+
+    @ApiModelProperty(value = "orderId")
+    private Long orderId;
+
+    @ApiModelProperty(value = "partSn")
+    private String partSn;
+
+    @ApiModelProperty(value = "支付密码")
+    @NotBlank(message = "支付密码不能为空")
+    private String password;
+
+    @ApiModelProperty(value = "特权劵id")
+    private List<Integer> privilegeId;
+
+    @ApiModelProperty(value = "优惠券id，如果没使用优惠券可不填写")
+    private List<Integer> userCouponId;
+}
