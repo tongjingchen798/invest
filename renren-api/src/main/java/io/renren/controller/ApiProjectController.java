d3b8199e131ec9840ec16dbfc1d1f92c7c2067eb
nico <nico@example.com>
2025-08-08 01:31:56 +0800

    新增投资项目类型列表接口

--- renren-api/src/main/java/io/renren/controller/ApiProjectController.java
+++ renren-api/src/main/java/io/renren/controller/ApiProjectController.java
@@ -1,40 +1,44 @@
-/**
- * Copyright (c) 2018 人人开源 All rights reserved.
- *
- * https://www.renren.io
- *
- * 版权所有，侵权必究！
- */
 
-package io.renren.controller;
+ package io.renren.controller;
 
-import io.renren.common.utils.Result;
-import io.renren.dto.ProjectDTO;
-import io.renren.service.ProjectService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import javax.annotation.Resource;
-import java.util.List;
-
-/**
- * 投资项目接口
- *
- * @author Mark sunlightcs@gmail.com
- */
-@RestController
-@RequestMapping("/api/project")
-@Api(tags = "投资项目接口")
-public class ApiProjectController {
-    @Resource
-    private ProjectService projectService;
-
-    @GetMapping("listGroup")
-    @ApiOperation("投资项目列表")
-    public Result<List<ProjectDTO>> listGroup() {
-        List<ProjectDTO> list = projectService.queryListGroup();
-        return new Result<List<ProjectDTO>>().ok(list);
-    }
-}
+ import io.renren.common.utils.Result;
+ import io.renren.dto.ProjectDTO;
+ import io.renren.dto.ProjectTypeDTO;
+ import io.renren.service.ProjectService;
+ import io.renren.service.ProjectTypeService;
+ import io.swagger.annotations.Api;
+ import io.swagger.annotations.ApiOperation;
+ import org.springframework.web.bind.annotation.*;
+ 
+ import javax.annotation.Resource;
+ import java.util.List;
+ 
+ /**
+  * 投资项目接口
+  *
+  * @author Mark sunlightcs@gmail.com
+  */
+ @RestController
+ @RequestMapping("/api/project")
+ @Api(tags = "投资项目接口")
+ public class ApiProjectController {
+     @Resource
+     private ProjectService projectService;
+     
+     @Resource
+     private ProjectTypeService projectTypeService;
+ 
+     @GetMapping("listGroup")
+     @ApiOperation("投资项目列表")
+     public Result<List<ProjectDTO>> listGroup() {
+         List<ProjectDTO> list = projectService.queryListGroup();
+         return new Result<List<ProjectDTO>>().ok(list);
+     }
+     
+     @GetMapping("list")
+     @ApiOperation("投资项目类型列表")
+     public Result<List<ProjectTypeDTO>> list() {
+         List<ProjectTypeDTO> list = projectTypeService.queryList();
+         return new Result<List<ProjectTypeDTO>>().ok(list);
+     }
+ }
