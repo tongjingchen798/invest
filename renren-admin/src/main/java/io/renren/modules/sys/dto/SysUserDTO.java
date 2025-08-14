b6cc2a24cba148d43a227a4c6e5dc1ad08778eda
nico <nico@example.com>
2025-08-14 21:45:42 +0800

    fix

--- renren-admin/src/main/java/io/renren/modules/sys/dto/SysUserDTO.java
+++ renren-admin/src/main/java/io/renren/modules/sys/dto/SysUserDTO.java
@@ -1,137 +1,87 @@
- 
+
 
 package io.renren.modules.sys.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.renren.common.validator.group.AddGroup;
+import io.renren.common.validator.group.DefaultGroup;
+import io.renren.common.validator.group.UpdateGroup;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 
 /**
- * 系统用户DTO
+ * 用户管理
  *
- * @author renren
- * @since 2024-01-01
+ * @author Mark sunlightcs@gmail.com
+ * @since 1.0.0
  */
 @Data
-public class SysUserDTO {
+@ApiModel(value = "用户管理")
+public class SysUserDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
 
-    /**
-     * 用户ID
-     */
+    @ApiModelProperty(value = "id")
+    @Null(message="{id.null}", groups = AddGroup.class)
+    @NotNull(message="{id.require}", groups = UpdateGroup.class)
     private Long id;
 
-    /**
-     * 用户名
-     */
+    @ApiModelProperty(value = "用户名", required = true)
+    @NotBlank(message="{sysuser.username.require}", groups = DefaultGroup.class)
     private String username;
 
-    /**
-     * 姓名
-     */
+    @ApiModelProperty(value = "密码")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @NotBlank(message="{sysuser.password.require}", groups = AddGroup.class)
+    private String password;
+
+    @ApiModelProperty(value = "姓名", required = true)
+    @NotBlank(message="{sysuser.realname.require}", groups = DefaultGroup.class)
     private String realName;
 
-    /**
-     * 头像
-     */
+    @ApiModelProperty(value = "头像")
     private String headUrl;
 
-    /**
-     * 性别   0：男   1：女    2：保密
-     */
+    @ApiModelProperty(value = "性别   0：男   1：女    2：保密", required = true)
+    @Range(min=0, max=2, message = "{sysuser.gender.range}", groups = DefaultGroup.class)
     private Integer gender;
 
-    /**
-     * 邮箱
-     */
+    @ApiModelProperty(value = "邮箱")
+    @Email(message="{sysuser.email.error}", groups = DefaultGroup.class)
     private String email;
 
-    /**
-     * 手机号
-     */
+    @ApiModelProperty(value = "手机号")
     private String mobile;
 
-    /**
-     * 部门ID
-     */
+    @ApiModelProperty(value = "部门ID", required = true)
+    @NotNull(message="{sysuser.deptId.require}", groups = DefaultGroup.class)
     private Long deptId;
 
-    /**
-     * 状态  0：停用   1：正常
-     */
+    @ApiModelProperty(value = "状态  0：停用    1：正常", required = true)
+    @Range(min=0, max=1, message = "{sysuser.status.range}", groups = DefaultGroup.class)
     private Integer status;
 
-    /**
-     * 超级管理员   0：否   1：是
-     */
-    private Integer superAdmin;
-
-    /**
-     * 创建时间
-     */
+    @ApiModelProperty(value = "创建时间")
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Date createDate;
 
-    /**
-     * 渠道
-     */
-    private String channel;
+    @ApiModelProperty(value = "超级管理员   0：否   1：是")
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Integer superAdmin;
 
-    /**
-     * 类型
-     */
-    private Integer type;
+    @ApiModelProperty(value = "角色ID列表")
+    private List<Long> roleIdList;
 
-    /**
-     * 代理ID
-     */
-    private Long agent;
-
-    /**
-     * 域名
-     */
-    private String domainname;
-
-    /**
-     * 下载码
-     */
-    private String dowloadCode;
-
-    /**
-     * 微信图片
-     */
-    private String wsimage;
-
-    /**
-     * 微信号
-     */
-    private String wsnumber;
-
-    /**
-     * 微信名称
-     */
-    private String wsname;
-
-    /**
-     * 推广号
-     */
-    private String tgnumber;
-
-    /**
-     * 二次验证码
-     */
-    private String twoFactorCode;
-
-    /**
-     * 代理邀请码
-     */
-    private String agentInviteCode;
-
-    /**
-     * 部门名称
-     */
+    @ApiModelProperty(value = "部门名称")
     private String deptName;
 
-    /**
-     * 角色ID列表
-     */
-    private Long[] roleIdList;
 }
