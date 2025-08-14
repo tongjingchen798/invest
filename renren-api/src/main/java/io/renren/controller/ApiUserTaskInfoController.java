7d841c38aea1c9ae0f69064e084a3b430dc9cc2b
nico <nico@example.com>
2025-08-14 23:13:50 +0800

    fix

--- renren-api/src/main/java/io/renren/controller/ApiUserTaskInfoController.java
+++ renren-api/src/main/java/io/renren/controller/ApiUserTaskInfoController.java
@@ -1,16 +1,17 @@
 package io.renren.controller;
 
+import io.renren.annotation.Login;
+import io.renren.annotation.LoginUser;
 import io.renren.common.utils.Result;
 import io.renren.dto.UserTaskInfoDTO;
+import io.renren.entity.UserEntity;
 import io.renren.service.UserTaskInfoService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -29,27 +30,14 @@ public class ApiUserTaskInfoController {
     @Autowired
     private UserTaskInfoService userTaskInfoService;
 
+    @Login
     @GetMapping("userTaskInfo")
     @ApiOperation("获取用户任务完成信息")
-    public Result getUserTaskInfo(
-            @ApiParam(value = "用户ID", required = false) 
-            @RequestParam(value = "userId", required = false) Long userId) {
-        
+    public Result getUserTaskInfo(@LoginUser UserEntity user) {
         try {
-            UserTaskInfoDTO taskInfo;
-            
-            if (userId != null) {
-                // 如果提供了用户ID，获取指定用户的任务信息
-                taskInfo = userTaskInfoService.getUserTaskInfo(userId);
-            } else {
-                // 如果没有提供用户ID，获取当前登录用户的任务信息
-                taskInfo = userTaskInfoService.getCurrentUserTaskInfo();
-            }
-            
+            UserTaskInfoDTO taskInfo = userTaskInfoService.getUserTaskInfo(user.getId());
             return new Result().ok(taskInfo);
-            
         } catch (Exception e) {
-            log.error("获取用户任务信息失败，用户ID: {}", userId, e);
             return new Result().error("获取用户任务信息失败: " + e.getMessage());
         }
     }
